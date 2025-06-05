59679ec75969fc0fd452cc836818bddd46b52c82
Patryk Łyko <plyko90@icloud.com>
2025-06-05 14:28:40 +0200

    patch

--- js/svg-loader.js
+++ js/svg-loader.js
@@ -1,14 +1,14 @@
  // ### WCZYTUJE PLIKI SVG DO KODU HTML ###
  
  // Wczytaj plik SVG github-mark.svg
-  fetch('/img/github-mark.svg')
+  fetch('./img/github-mark.svg')
     .then(response => response.text())
     .then(svgData => {
       document.getElementById('github-icon').innerHTML = svgData;
     });
 
       // Wczytaj plik SVG sun-moon.svg
-  fetch('/img/sun-moon.svg')
+  fetch('./img/sun-moon.svg')
     .then(response => response.text())
     .then(svgData => {
       document.getElementById('theme-toggle').innerHTML = svgData;
